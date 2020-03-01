664026e8e48b44c056587ba4220f998e08adf263
Max Strehle <max-aero120@users.noreply.github.com>
2020-02-29 20:03:03 -0600

    Add files via upload

---
+++ Bigbrain.py
@@ -0,0 +1,17 @@
+## Gas Dynamics Project 1\\John Kloser & Maxim Strehle\\03/06/2020
+##Prandtl-Meyer Flow
+##
+import numpy as np
+import scipy and sy
+import matplotlib.pyplot as plot
+import sys
+
+M1 = 3.0
+P1 = 8.7023 #psia
+P3 = 10.1527 #psia
+
+for 
+
+del_0 = 25.5
+
+Beta
