46b8465d4f107818e2a030e92cd02da7e3a41cc5
jkloser <john.kloser3@gmail.com>
2020-03-03 20:53:16 -0600

    fixed delta iterations

--- Bigbrain.py
+++ Bigbrain.py
@@ -14,13 +14,13 @@ gamma = 1.4
 P01 = (1/0.02722) * P1
 max_del = 0.9*(M1 - 1)**(3/2)
 del_0 = 0
-for delta in np.arange(del_0, max_del, 0.00001):
+for delta in np.arange(del_0, 45*np.pi/180, 0.0001):
 
     #Beta
     error_beta = 10
     beta_max = np.pi/2
     beta_min = 0.0
-    while abs(error_beta) > 0.0001:
+    while abs(error_beta) > 0.001:
         beta = (beta_max + beta_min)/2
         error_beta = ((M1**2 * (np.sin(beta))**2 - 1) * sp.cot(beta)) / (((gamma+1)/2) * M1**2 - M1**2 * (np.sin(beta))**2 + 1) - np.tan(delta)
 
@@ -53,16 +53,17 @@ for delta in np.arange(del_0, max_del, 0.00001):
     y = (nu3/nu_inf)**(2/3)
     M3 = (1 + A*y + B*y**2 + C*y**3)/(1 + D*y + E*y**2)
 
-    error_M3 = 10
-    while abs(error_M3) > 0.01:
-        f = np.sqrt(gamma_ratio) * np.arctan(np.sqrt((1/gamma_ratio) * (M3**2 - 1))) - np.arctan(M3**2 - 1)
+    #Newton Iterations
+    f = 10
+    while abs(f) > 0.001:
+        f = np.sqrt(gamma_ratio) * np.arctan(np.sqrt((1/gamma_ratio) * (M3**2 - 1))) - np.arctan(np.sqrt(M3**2 - 1)) - nu3
         df = gamma_ratio * np.sqrt(gamma_ratio)*M3 / (M3**2 - 1 + gamma_ratio * np.sqrt(M3**2 - 1)) - (1/(M3*np.sqrt(M3**2 -1)))
-        M3 = M3 - (f-nu3)/df
-        error_M3 = (f-nu3)
-        print('M3 loop')
-
+        M3 = M3 - f/df
     P3_ratio = (1+(gamma-1)*M3**2/2)**(gamma/(gamma-1)) #P03/P3
     P3_guess = (1/P3_ratio)*P02 #P02 = P03
-    if abs(P3-P3_guess) < 0.1:
+    if abs(P3-P3_guess) < 0.01:
+        print(M3)
+        print(delta)
         break
-print(M3)
+print (M3)
+print(delta)
--- test_file.py
+++ test_file.py
@@ -1,20 +1,32 @@
 import numpy as np
 import sympy as sp
 delta = 8*np.pi/180
-M1 = 3.0
+M2 = 3.0
 gamma = 1.4
-#Beta
-error_beta = 10
-beta_max = 2.5
-beta_min = 0.0
+gamma_ratio = (gamma+1)/(gamma-1)
+nu2 = np.sqrt(gamma_ratio) * np.arctan(np.sqrt((1/gamma_ratio) * (M2**2 - 1))) - np.arctan(np.sqrt(M2**2 - 1))
+nu3 = delta + nu2
 
-while abs(error_beta) > 0.0001:
-    beta = (beta_max + beta_min)/2
-    error_beta = ((M1**2 * (np.sin(beta))**2 - 1) * sp.cot(beta)) / (((gamma+1)/2) * M1**2 - M1**2 * (np.sin(beta))**2 + 1) - np.tan(delta)
+#Hall Inverse Approximation
+A = 1.3604
+B = 0.0962
+C = -0.5127
+D = -0.6722
+E = -0.3278
+nu_inf = np.pi/2 * (np.sqrt(6) - 1)
+y = (nu3/nu_inf)**(2/3)
+M3 = (1 + A*y + B*y**2 + C*y**3)/(1 + D*y + E*y**2)
 
-    if error_beta > 0:
-        beta_max = beta
-    else:
-        beta_min = beta
-
-print(beta*180/np.pi)
+#Newton Iterations
+f = 10
+while abs(f) > 0.001:
+    f = np.sqrt(gamma_ratio) * np.arctan(np.sqrt((1/gamma_ratio) * (M3**2 - 1))) - np.arctan(np.sqrt(M3**2 - 1)) - nu3
+    #df = beta*(1-gamma**2)/(M3*(1+gamma**2*beta**2))
+    df = gamma_ratio * np.sqrt(gamma_ratio)*M3 / (M3**2 - 1 + gamma_ratio * np.sqrt(M3**2 - 1)) - (1/(M3*np.sqrt(M3**2 -1)))
+    M3 = M3 - f/df
+print(delta)
+#P3_ratio = (1+(gamma-1)*M3**2/2)**(gamma/(gamma-1)) #P03/P3
+#P3_guess = (1/P3_ratio)*P02 #P02 = P03
+#if abs(P3-P3_guess) < 0.01:
+#    print(M3)
+#    print(delta)
